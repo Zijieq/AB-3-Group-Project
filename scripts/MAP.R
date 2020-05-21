6dba0e383ce14e887ca73a4ef33793104ba8a3cd
Zijie Qiu <zijieq@uw.edu>
2020-05-21 16:23:48 -0700

    adding new data set into data/ file and correcting the code for efficiency

--- scripts/MAP.R
+++ scripts/MAP.R
@@ -10,36 +10,10 @@ library("ggplot2")
 library("ggalt")
 library("leaflet")
 
-# connect API key with R
-# register_google(key = "AIzaSyBuvi7L7rY8RLZSlXLFoq8YULs-rXy_7qw", write = TRUE)
-
-# reading the dataset
-# traffic_collision <- read.csv("data/Collisions.csv", stringsAsFactors = FALSE)
-# 
-# # typeof(pull(traffic_collision, location))
-# 
-# lat_long_column <- traffic_collision %>%
-#   filter(substring(INCDATE, 1, 4) == "2017") %>%
-#   mutate_geocode(LOCATION)
-# 
-# # Washigotn state traffic status
-# leaflet(data = lat_long_column) %>%
-#   addProviderTiles("CartoDB.Positron") %>%
-#   setView(lat = 47.6, lng = -122.3, zoom = 10) %>%
-#   addMarkers(
-#     clusterOptions = markerClusterOptions()
-#   )
-
-
-
+# drawing a map with following function
 
 draw_map <- function(dataframe) {
-  lat_long_column <- dataframe %>%
-    filter(substring(INCDATE, 1, 4) == "2017") %>%
-    mutate_geocode(LOCATION) %>%
-    filter(!is.na(lon) & !is.na(lat))
-  
-    map <- leaflet(data = lat_long_column) %>%
+    map <- leaflet(dataframe) %>%
     addProviderTiles("CartoDB.Positron") %>%
     setView(lat = 47.6, lng = -122.3, zoom = 10) %>%
     addMarkers(
